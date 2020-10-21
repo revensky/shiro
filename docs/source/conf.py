b7aeec5d93633f67ffd1d471a265dc4886ec6f81
Eduardo Rezende <eduardorbr7@gmail.com>
2020-10-21 20:14:12 -0300

    Fixed installation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Eduardo Ribeiro Rezende"
 author = "Eduardo Ribeiro Rezende"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
